9df92c7a2aa9bd786d84c6a2352b73da2604a251
dxsharek077@gmail.com <sharekovi@Shareks-MacBook-Pro.local>
2023-05-17 13:04:00 -0400

    HW2

---
+++ features/steps/HW2 Test Case.py
@@ -0,0 +1,41 @@
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://www.amazon.com/')
+driver.find_element(By.ID,'nav-orders').click()
+
+expected_text = 'Sign in'
+
+actual_text = driver.find_element(By.XPATH,"//h1[@class='a-spacing-small']").text
+
+assert actual_text == expected_text,f'Expected {expected_text} but go {actual_text}'
+
+assert driver.find_element(By.ID,'ap_email').is_displayed(), 'Email field not shown'
+
+driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
