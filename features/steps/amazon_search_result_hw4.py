a5cde30a8e4d75387a72f14c6c993c1001ce8a98
dxsharek077@gmail.com <sharekovi@Shareks-MacBook-Pro.local>
2023-06-10 00:10:51 -0400

    HW4

--- features/steps/amazon_search_result_hw4.py
+++ features/steps/amazon_search_result_hw4.py
@@ -1,16 +1,17 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
-RESULT_TEXT= (By.XPATH,"//span[@class='a-color-state a-text-bold']")
-PRODUCT_PRICE=(By.XPATH,"//div[@data-component-type='s-search-result']//a[.//span[@class='a-price']]")
+
+RESULT_TEXT = (By.XPATH, "//span[@class='a-color-state a-text-bold']")
+PRODUCT_PRICE = (By.XPATH, "//div[@data-component-type='s-search-result']//a[.//span[@class='a-price']]")
+
 
 @then('Verify search result shown for{expected_result}')
-def very_search_result(context,expected_result):
-    actual_result= context.driver.find_element(*RESULT_TEXT).text
+def very_search_result(context, expected_result):
+    actual_result = context.driver.find_element(*RESULT_TEXT).text
     assert expected_result == actual_result, f'Error ! Expected {expected_result} bot got actual {actual_result}'
 
-    @when('Click on the first product')
-    def click_first_product(context):
-        context.driver.find_element(*PRODUCT_PRICE).click()
 
-
+@when('Click on the first product')
+def click_first_product(context):
+    context.driver.find_element(*PRODUCT_PRICE).click()
--- features/steps/product_page_hw4.py
+++ features/steps/product_page_hw4.py
@@ -1,15 +1,17 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
+
 ADD_TO_CART_BTN = (By.ID, 'add-to-cart-button')
-PRODUCT_NAME = (By.ID,  'productTitle')
+PRODUCT_NAME = (By.ID, 'productTitle')
 
 
 @when('Click on Add to cart button')
 def click_add_to_cart(context):
     context.driver.find_element(*ADD_TO_CART_BTN).click()
 
-    @when('Store product name')
-    def get_product_name(context):
-        context.product_name = context.driver.find_element(*PRODUCT_NAME).text
-        print(f'Current product:{context.product_name}')
+
+@when('Store product name')
+def get_product_name(context):
+    context.product_name = context.driver.find_element(*PRODUCT_NAME).text
+    print(f'Current product:{context.product_name}')
